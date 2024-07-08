2929f992b9726f8e68d41fb91ad3c33c44004430
24기구원 <koowonqwe2@gmail.com>
2024-07-08 14:01:30 +0900

    Database

--- teamname/Assets/Scripts/rndnjs/Database.cs
+++ teamname/Assets/Scripts/rndnjs/Database.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "PlayerData", menuName = "player Data", order = 51)]
@@ -8,6 +9,14 @@ public class Database : ScriptableObject
     public string name;
     public float attack;
     public float defense;
+    public float CriticalDamage;
+    public float CriticalChance;
+    public float moveSpeed;
+    public float atkSpeed;
+    public float Vamp;
+    public float DmgMultiplier;
+
+
 
 
 }
