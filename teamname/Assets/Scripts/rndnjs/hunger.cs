1ea7cfbf5892fa17eb61d5144cfd144a389fceea
24기구원 <koowonqwe2@gmail.com>
2024-07-08 14:28:06 +0900

    trash

---
+++ teamname/Assets/Scripts/rndnjs/hunger.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Numerics;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class hunger : MonoBehaviour
+{
+    public Database move;
+    float[] arra = new float[5] { 1.1f, 1.2f, 1.3f, 1.4f, 1.5f };
+    int a=0;
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            a++;
+        }
+        move.moveSpeed *= arra[a];
+        
+    }
+
+    
+    
+}
