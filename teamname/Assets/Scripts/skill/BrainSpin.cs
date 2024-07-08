cd07d20fa8c2ff04e5384fbe3ff3c21022bfff2f
Nevil0713 <128784519+Nevil0713@users.noreply.github.com>
2024-07-08 16:06:06 +0900

    skill3

--- teamname/Assets/Scripts/skill/BrainSpin.cs
+++ teamname/Assets/Scripts/skill/BrainSpin.cs
@@ -12,7 +12,12 @@ public class BrainSpin : MonoBehaviour
     bool isMaxLv = false;
     float[] Rate = new float[6] { 1, 1.1f, 1.2f, 1.3f, 1.4f, 1.5f };
 
-    void LvUP()
+    public void Select()
+    {
+        LvUP();
+    }
+
+    private void LvUP()
     {
         Lv++;
         playerData.attack = playerData.ATK*Rate[Lv];
