588d4c624ca055a1a2d8fb0ef4ec2f382f2dbbf8
112vasd <a49458447@gmail.com>
2024-07-08 17:00:53 +0900

    .

--- teamname/Assets/Scripts/skill/LuckyVicky.cs
+++ teamname/Assets/Scripts/skill/LuckyVicky.cs
@@ -12,7 +12,7 @@ public class LuckyVicky : MonoBehaviour
     int maxLv = 5;
     int Lv = 0;
     bool isMaxLv = false;
-    float[] Rate = new float[5] { 1.05f, 3.15f, 6.3f, 10.5f, 15.75f };
+    float[] Rate = new float[6] { 1, 1.05f, 1.1f, 1.15f, 1.2f, 1.25f };
 
     public void Select()
     {
@@ -22,7 +22,7 @@ public class LuckyVicky : MonoBehaviour
     private void LvUP()
     {
         Lv++;
-        playerData.defense = Rate[Lv];
+        playerData.defense = playerData.DEF * Rate[Lv];
         if (Lv == maxLv)
         {
             isMaxLv = true;
