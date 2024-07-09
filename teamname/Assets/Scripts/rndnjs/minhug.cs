c4c38edbf0b7066dc5d72a777c0f2d2a55974d75
24기구원 <koowonqwe2@gmail.com>
2024-07-09 19:47:49 +0900

    attack motion

---
+++ teamname/Assets/Scripts/rndnjs/minhug.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class minhug : MonoBehaviour
+{
+    private Animator Minhug;
+
+    void Start()
+    {
+        Minhug = GetComponent<Animator>();
+        if (Minhug == null)
+        {
+            Debug.LogError("Animator component not found on " + gameObject.name);
+        }
+    }
+
+    void FixedUpdate()
+    {
+        // FixedUpdate is called once per frame
+        attack();
+    }
+
+    void attack()
+    {
+        // 여기에 추가적인 공격 로직이 필요하다면 추가할 수 있습니다.
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("player"))
+        {
+            Debug.Log("Player entered trigger zone.");
+            if (Minhug != null)
+            {
+                Minhug.SetBool("attack", true);
+                Debug.Log("Attack set to true.");
+            }
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D collision)
+    {   
+        if (collision.CompareTag("player"))
+        {
+            Debug.Log("Player exited trigger zone.");
+            if (Minhug != null)
+            {
+                Minhug.SetBool("attack", false);
+                Debug.Log("Attack set to false.");
+            }
+        }
+    }
+}
