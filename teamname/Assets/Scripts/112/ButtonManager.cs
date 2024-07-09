0ab9c61bc5c9f950d01e28c592f2701267d2650f
112vasd <a49458447@gmail.com>
2024-07-09 19:48:09 +0900

    .

---
+++ teamname/Assets/Scripts/112/ButtonManager.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+
+public class ButtonManager : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject[] Skills; // 스킬 프리팹 배열
+    public GameObject s; // 버튼들이 추가될 부모 오브젝트
+
+    private void Awake()
+    {
+        List<int> chosenIndices = new List<int>(); // 선택된 인덱스를 저장할 리스트
+        int a = 0;
+
+        for (int i = 0; i < 3; ++i)
+        {
+            int index;
+            do
+            {
+                index = Random.Range(0, Skills.Length);
+            } while (chosenIndices.Contains(index)); // 중복되지 않는 인덱스를 찾을 때까지 반복
+
+            chosenIndices.Add(index); // 선택된 인덱스를 리스트에 추가
+
+            GameObject btn = Instantiate(Skills[index], new Vector3(0, a, 0), Quaternion.identity);
+            btn.transform.SetParent(s.transform);
+            ++a;
+        }
+    }
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+}
--- teamname/Assets/Scripts/112/SelectSkill.cs
+++ teamname/Assets/Scripts/112/SelectSkill.cs
@@ -13,23 +13,23 @@ public class SelectSkill : MonoBehaviour
     private bool isChoose = false;
 
     public Text text;
+
     public void ColorChange()
     {
         ColorBlock colorBlock = button.colors;
 
         isChoose = !isChoose; 
 
+        colorBlock.normalColor = isChoose ? Color.white : new Color(0f, 0f, 0f, 0f);
+        colorBlock.selectedColor = isChoose ? Color.white : new Color(0f, 0f, 0f, 0f);
 
-        colorBlock.normalColor = isChoose ? new Color(1f, 1f, 1f, 1f) : Color.white;
-        colorBlock.selectedColor = isChoose ? new Color(1f, 1f, 1f, 1f) : Color.white;
-
-        text.text = "<color=black>skill</color>";
         button.colors = colorBlock;
     }
 
+
     void Start()
     {
-        transform.DOLocalMoveY(moveAmount, 0.5f).SetEase(Ease.InQuad).OnComplete(ColorChange);
+       // transform.DOLocalMoveY(moveAmount, 0.7f).SetEase(Ease.InQuad).OnComplete(ColorChange);
     }
 
     public void ButtonClick()
