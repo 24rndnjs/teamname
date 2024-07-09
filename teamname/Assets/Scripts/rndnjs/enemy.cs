be1cb91d27fb9a2039d5e0bd3fce56747e1d635d
24기구원 <koowonqwe2@gmail.com>
2024-07-09 13:31:36 +0900

    enemy구현

--- teamname/Assets/Scripts/rndnjs/enemy.cs
+++ teamname/Assets/Scripts/rndnjs/enemy.cs
@@ -7,48 +7,59 @@ public class enemy : MonoBehaviour
     public EnenmyDatabase enemydata;
     public float currenthp;
     public float space;
-    public GameObject closetplayer;
+    public GameObject closestPlayer;
+    private bool isFlipped = false;
 
     void Start()
     {
         currenthp = enemydata.hp;
     }
 
-
     void Update()
     {
-        GameObject [] players = GameObject.FindGameObjectsWithTag("player");
+        GameObject[] players = GameObject.FindGameObjectsWithTag("player");
         FindClosestPlayer(players);
-        if (closetplayer != null)
+        if (closestPlayer != null)
         {
-
-            Vector3 direction = closetplayer.transform.position - transform.position;
+            Vector3 direction = closestPlayer.transform.position - transform.position;
             float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
-            transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle));
-            transform.position = Vector2.MoveTowards(this.transform.position, closetplayer.transform.position, enemydata.speed * Time.deltaTime);
-            float distance = Vector2.Distance(transform.position, closetplayer.transform.position);
 
+            if (!isFlipped)
+            {
+                transform.localScale = new Vector3(-transform.localScale.x, transform.localScale.y, transform.localScale.z);
+                isFlipped = true;
+            }
+
+            transform.position = Vector2.MoveTowards(this.transform.position, closestPlayer.transform.position, enemydata.speed * Time.deltaTime);
+
+            float distance = Vector2.Distance(transform.position, closestPlayer.transform.position);
+            if (distance <= space)
+            {
+                transform.localScale = new Vector3(-transform.localScale.x, transform.localScale.y, transform.localScale.z);
+                isFlipped = false;
+            }
         }
     }
+
     void FindClosestPlayer(GameObject[] players)
     {
         float minDistance = Mathf.Infinity;
-        closetplayer = null;
+        closestPlayer = null;
 
         foreach (GameObject player in players)
         {
             float distance = Vector2.Distance(transform.position, player.transform.position);
-            if (distance <= space && distance < minDistance)
+            if (distance < minDistance)
             {
                 minDistance = distance;
-                closetplayer = player;
+                closestPlayer = player;
             }
         }
     }
+
     void OnDrawGizmosSelected()
     {
         Gizmos.color = Color.red;
         Gizmos.DrawWireCube(transform.position, new Vector3(space, space, space));
-
     }
 }
