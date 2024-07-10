103cc674d288c46af2ab806d8f4c85e19c9e3927
112vasd <a49458447@gmail.com>
2024-07-10 14:50:33 +0900

    .

--- teamname/Assets/Scripts/112/SelectSkill.cs
+++ teamname/Assets/Scripts/112/SelectSkill.cs
@@ -6,38 +6,14 @@ using UnityEngine.UI;
 using DG.Tweening;
 public class SelectSkill : MonoBehaviour
 {
-    public Button[] buttons = new Button[3];
-    public string[] buttoncheck = new string[21] { "skill 1", " skill 2", " skill 3", "skill 4", "skill 5", "skill 6", "skill 7", "skill 8", "skill 9", " skill 10", " skill 11", " skill 12", "skill 13", " skill 14", " skill 15", "skill 16", "skill 17", "skill 18", "skill 19", "skill 20", "skill 21" };
-    public int[] buttoncount = new int[21] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
-    public Button button;
+
 
     private bool isChoose = false;
    
-    public Text text;
+
     
     void Start()
     {
-        button.onClick.AddListener(ButtonClick);
-        transform.DOLocalMoveY(90, 0.7f).SetEase(Ease.InQuad).SetRelative();
-    }
-
-    public void ButtonClick()
-    {
-        Debug.Log("Clicked");
-        OnClickSkillPerk();
-        string EventButtonName = EventSystem.current.currentSelectedGameObject.name;
-        for (int i = 0; i < 21; ++i)
-        {
-            if (buttoncheck[i]==EventButtonName)
-            {
-                ++buttoncount[i];
-            }
-        }
-
-        Destroy(button.gameObject);
-    }
-    public void OnClickSkillPerk()
-    {
-        Debug.Log("LVUP");
+   
     }
 }
