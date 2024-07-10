bd6df2ed535cce57ca96715c1b7697c0fa2270a2
Nevil0713 <128784519+Nevil0713@users.noreply.github.com>
2024-07-10 19:05:21 +0900

    오류수정

--- teamname/Assets/Scripts/Nev/GameManager.cs
+++ teamname/Assets/Scripts/Nev/GameManager.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     public static GameManager instance;
-    public player player;
+    public PlayerController player;
     public PoolManager pool;
 
     private void Awake()
